b6ff8cfb4bbc1d720fc8f1b82d30d876b38fa8b3
BuildTools <unconfigured@null.spigotmc.org>
2023-04-07 12:37:58 +0200

    Modify MySQL stateful

--- MySQL-DB/schema.sql
+++ MySQL-DB/schema.sql
@@ -1,6 +1,11 @@
+DROP DATABASE IF EXISTS flask_blog;
+
 CREATE DATABASE flask_blog;
 
 USE flask_blog;
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
 
 CREATE TABLE tbl_user (
   user_id BIGINT NULL AUTO_INCREMENT,
@@ -8,11 +13,12 @@ CREATE TABLE tbl_user (
   user_username VARCHAR(45) NULL,
   user_password VARCHAR(45) NULL,
   PRIMARY KEY (user_id)
+  UNIQUE (user_username)
 );
 
-INSERT INTO tbl_user (user_name, user_username, user_password) VALUES ('Alex', 'user_alex', '1234');
-INSERT INTO tbl_user (user_name, user_username, user_password) VALUES ('Manuel', 'user_alex', '5678');
-INSERT INTO tbl_user (user_name, user_username, user_password) VALUES ('Pedro', 'user_alex', '8910');
+-- INSERT INTO tbl_user (user_name, user_username, user_password) VALUES ('Alex', 'user_alex', '1234');
+-- INSERT INTO tbl_user (user_name, user_username, user_password) VALUES ('Manuel', 'user_alex', '5678');
+-- INSERT INTO tbl_user (user_name, user_username, user_password) VALUES ('Pedro', 'user_alex', '8910');
 
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -23,9 +29,9 @@ CREATE TABLE posts (
     FOREIGN KEY (author_id) REFERENCES tbl_user(user_id)
 );
 
-INSERT INTO posts (title, content) VALUES ( 'Post de Comida', 'Comidas ricas');
-INSERT INTO posts (title, content) VALUES ( 'Post de Ropa', 'Ropa a la moda');
-INSERT INTO posts (title, content) VALUES ( 'Post de Coches', 'Coches lentos');
+-- INSERT INTO posts (title, content) VALUES ( 'Post de Comida', 'Comidas ricas');
+-- INSERT INTO posts (title, content) VALUES ( 'Post de Ropa', 'Ropa a la moda');
+-- INSERT INTO posts (title, content) VALUES ( 'Post de Coches', 'Coches lentos');
 
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createUser`(
