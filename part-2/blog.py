32343c79425b015257cb18372dc511dd6eadeccb
BuildTools <unconfigured@null.spigotmc.org>
2023-04-15 18:29:32 +0200

    fix path to templates

--- part-2/blog.py
+++ part-2/blog.py
@@ -75,7 +75,7 @@ def create():
             db.commit()
             return redirect(url_for("blog.index"))
 
-    return render_template("blog/create.html")
+    return render_template("create.html")
 
 
 @bp.route("/<int:id>/update", methods=("GET", "POST"))
@@ -104,7 +104,7 @@ def update(id):
             db.commit()
             return redirect(url_for("blog.index"))
 
-    return render_template("blog/update.html", post=post)
+    return render_template("update.html", post=post)
 
 
 @bp.route("/<int:id>/delete", methods=("POST",))
