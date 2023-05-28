198a75b39ae2f8a76b67dc5593add43ebaa87a96
BuildTools <unconfigured@null.spigotmc.org>
2023-05-28 17:56:43 +0200

    Fixing test.py url

--- part-2/test.py
+++ part-2/test.py
@@ -1,47 +1,48 @@
 import pytest
 from flask import g, session
 from .db import get_db
+from .auth import login, login_required
 
 
-def test_index(client, auth):
+def test_index(client):
     # test that index page requires authentication
     response = client.get("/")
     assert b"Log In" in response.data
 
     # test that posts show up on index page
-    auth.login()
+    login()
     response = client.get("/")
     assert b"Test Title" in response.data
     assert b"Test Content" in response.data
     assert b"Test User" in response.data
 
 
-def test_create(client, auth):
+def test_create(client):
     # test that create page requires authentication
     response = client.get("/create")
-    assert response.headers["Location"] == "http://localhost/auth/login"
+    assert response.headers["Location"] == "http://localhost:5000/auth/login"
 
     # test creating a post
-    auth.login()
+    login()
     response = client.post("/create", data={"title": "created", "content": ""})
-    assert response.headers["Location"] == "http://localhost/blog/create"
+    assert response.headers["Location"] == "http://localhost:5000/blog/create"
 
     response = client.post(
         "/create", data={"title": "created", "content": "test content"}
     )
-    assert response.headers["Location"] == "http://localhost/blog/index"
+    assert response.headers["Location"] == "http://localhost:5000/blog/index"
     with client:
         response = client.get("/")
         assert b"created" in response.data
 
 
-def test_update(client, auth):
+def test_update(client):
     # test that update page requires authentication
     response = client.get("/3/update")
-    assert response.headers["Location"] == "http://localhost/auth/login"
+    assert response.headers["Location"] == "http://localhost:5000/auth/login"
 
     # test that non-authors cannot update a post
-    auth.login()
+    login()
     response = client.get("/2/update")
     assert b"Test Title" in response.data
     assert b"Test Content" in response.data
@@ -49,30 +50,30 @@ def test_update(client, auth):
 
     # test that authors can update a post
     response = client.post("/3/update", data={"title": "updated", "content": ""})
-    assert response.headers["Location"] == "http://localhost/blog/1/update"
+    assert response.headers["Location"] == "http://localhost:5000/blog/1/update"
 
     response = client.post(
         "/3/update", data={"title": "updated", "content": "test content"}
     )
-    assert response.headers["Location"] == "http://localhost/blog/index"
+    assert response.headers["Location"] == "http://localhost:5000/blog/index"
     with client:
         response = client.get("/")
         assert b"updated" in response.data
 
 
-def test_delete(client, auth):
+def test_delete(client):
     # test that delete route requires authentication
     response = client.post("/3/delete")
-    assert response.headers["Location"] == "http://localhost/auth/login"
+    assert response.headers["Location"] == "http://localhost:5000/auth/login"
 
     # test that non-authors cannot delete a post
-    auth.login()
+    login()
     response = client.post("/2/delete")
     assert response.status_code == 403
 
     # test that authors can delete a post
     response = client.post("/3/delete")
-    assert response.headers["Location"] == "http://localhost/blog/index"
+    assert response.headers["Location"] == "http://localhost:5000/blog/index"
     with client:
         response = client.get("/")
         assert b"Test Title" not in response.data
@@ -88,9 +89,10 @@ def test_delete(client, auth):
         ("/3/delete", 302),
     ),
 )
+@login_required
 def test_login_required(client, path, status_code):
     response = client.post(path)
-    assert response.headers["Location"] == "http://localhost/auth/login"
+    assert response.headers["Location"] == "http://localhost:5000/auth/login"
 
     response = client.get(path)
     assert response.status_code == status_code
